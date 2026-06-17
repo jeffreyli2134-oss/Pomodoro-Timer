27533912f842812b32bca80e812bf01cbc00b1b7
Jeffrey Li <jeffreyli2134@gmail.com>
2026-06-17 21:02:17 +1200

    Added background and proggress bar

--- function.js
+++ function.js
@@ -1,10 +1,13 @@
 let timer;
-let timeRemaining = 1500;
+let totalSeconds = 10;
+let timeRemaining = totalSeconds;
 let running = false;
 
 const timerElement = document.getElementById("timer");
 const startButton = document.getElementById("start");
 const resetButton = document.getElementById("reset");
+const pauseButton = document.getElementById("pause");
+const progress = document.querySelector(".progress");
 
 function updateTimer() {
     let minutes = Math.floor(timeRemaining / 60);
@@ -15,12 +18,14 @@ function updateTimer() {
     }
 
     timerElement.textContent = minutes + ":" + seconds;
+
+    let progressPercent = (timeRemaining / totalSeconds) * 100;
+    progress.style.width = progressPercent + "%";
 }
 
 updateTimer();
 
 startButton.addEventListener("click", function() {
-
     if (running) {
         return;
     }
@@ -28,7 +33,6 @@ startButton.addEventListener("click", function() {
     running = true;
 
     timer = setInterval(function() {
-
         timeRemaining--;
 
         if (timeRemaining <= 0) {
@@ -38,24 +42,17 @@ startButton.addEventListener("click", function() {
         }
 
         updateTimer();
-
     }, 1000);
-
 });
 
 resetButton.addEventListener("click", function() {
-
     clearInterval(timer);
     running = false;
-    timeRemaining = 1500; 
+    timeRemaining = totalSeconds;
     updateTimer();
 });
 
-const pauseButton = document.getElementById("pause");
-
 pauseButton.addEventListener("click", function() {
-
     clearInterval(timer);
     running = false;
-
 });
